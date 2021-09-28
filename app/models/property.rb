a5a89a759c08268560b90c07b9c7b75ae967508f
Rafael Briones <rbrionesdev@outlook.com>
2021-09-28 12:08:21 -0600

    available be done

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,4 +1,19 @@
 class Property < ApplicationRecord
   belongs_to :agent
   has_one :address
+
+  # SELECT p.id, p.beds, p.baths, p.sq_ft, p.agent_id, a.email, a.first_name, a.last_name, ad.city, ad.zip, p.sold
+  # FROM properties AS p
+  # INNER JOIN agents AS a ON a.id = p.agent_id
+  # INNER JOIN addresses AS ad ON ad.property_id = p.id
+  # WHERE p.sold <> TRUE
+  # ORDER by a.id
+  def self.available
+    select('p.id, p.beds, p.baths, p.sq_ft, p.agent_id, a.email, a.first_name, a.last_name, ad.city, ad.zip, p.sold')
+    .from('properties AS p')
+    .joins('INNER JOIN agents AS a ON a.id = p.agent_id
+          INNER JOIN addresses AS ad ON ad.property_id = p.id')
+    .where('p.sold <> TRUE')
+    .order('by a.id')
+  end
 end
